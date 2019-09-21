9b4199b16282163882a307bf01c5a20b09cd45cc
ThatcherDev <thatcher.clough@gmail.com>
2019-09-21 14:58:34 -0400

    Clean up

--- src/main/java/com/github/thatcherdev/socketshell/backend/Utils.java
+++ src/main/java/com/github/thatcherdev/socketshell/backend/Utils.java
@@ -50,40 +50,6 @@ public class Utils {
 	}
 
 	/**
-	 * Run {@link command} in Bash and return response.
-	 *
-	 * @param command command to run
-	 * @return response to running command
-	 */
-	public static String runBashCommand(String command) {
-		String resp = "";
-		BufferedReader bufferedReader = null;
-		try {
-			ProcessBuilder builder = new ProcessBuilder("bash", "-c", command);
-			builder.redirectErrorStream(true);
-			bufferedReader = new BufferedReader(new InputStreamReader(builder.start().getInputStream()));
-			while (true) {
-				String line = bufferedReader.readLine();
-				if (line == null)
-					break;
-				resp += line + "\n";
-			}
-			if (resp.isEmpty())
-				return "Command did not produce a response";
-			else
-				return resp.substring(0, resp.length() - 1);
-		} catch (Exception e) {
-			return "An error occurred when trying to run command";
-		} finally {
-			try {
-				if (bufferedReader != null)
-					bufferedReader.close();
-			} catch (Exception e) {
-			}
-		}
-	}
-
-	/**
 	 * Run PowerShell script {@link script}.
 	 *
 	 * @param script name of script to run
@@ -113,41 +79,6 @@ public class Utils {
 			return true;
 		} catch (Exception e) {
 			return false;
-		}
-	}
-
-	/**
-	 * @return SSID and password of current WiFi connection.
-	 */
-	public static String currentConnection() {
-		try {
-			String interfaceInfo = runCommand("c: && cd C:\\Windows\\System32 && netsh wlan show interfaces");
-			if (interfaceInfo.contains("Profile")) {
-				String interfaceLine = interfaceInfo.substring(interfaceInfo.lastIndexOf("Profile"));
-				String ssid = interfaceLine.substring(interfaceLine.indexOf(":") + 2, interfaceLine.indexOf("\n"));
-				String pass = getPass(ssid);
-				return "SSID:\t" + ssid + "\nPass:\t" + pass;
-			} else
-				throw new Exception();
-		} catch (Exception e) {
-			return "SSID:\tN/A\nPass:\tN/A";
-		}
-	}
-
-	/**
-	 * @param ssid SSID of network to get password of
-	 * @return password of WiFi network with SSID {@link ssid}
-	 */
-	private static String getPass(String ssid) {
-		try {
-			String ssidInfo = runCommand(
-					"c: && cd C:\\Windows\\System32 && netsh wlan show profiles " + ssid + " key=clear | findstr Key");
-			if (ssidInfo.contains("Key Content")) {
-				return ssidInfo.substring(ssidInfo.indexOf(":") + 2);
-			} else
-				throw new Exception();
-		} catch (Exception e) {
-			return "N/A";
 		}
 	}
 
